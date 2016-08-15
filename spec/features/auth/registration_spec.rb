461e6e8aafea8c9c07fb1a3aeb2f026ceb8a36d0
aniwarlock <aniwarlock@gmail.com>
2016-08-15 22:31:30 +0300

    auth

---
+++ spec/features/auth/registration_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+feature 'User registration', %q{
+  In order to ask questions and give answers
+  As a registered user
+  I want to be able to register
+} do
+
+  let(:user_attr) {attributes_for(:user)}
+  let(:user) {create(:user)}
+
+  scenario 'New user try to register' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user_attr[:email]
+    fill_in 'Password', with: user_attr[:password]
+    fill_in 'Password confirmation', with: user_attr[:password_confirmation]
+    click_on 'Sign up'
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+  end
+
+  scenario 'Registered user try to register' do
+    visit new_user_registration_path
+    fill_in 'Email', with: user.email
+    fill_in 'Password', with: user.password
+    fill_in 'Password confirmation', with: user.password_confirmation
+    click_on 'Sign up'
+
+    expect(page).to have_content('Email has already been taken')
+  end
+end
---
+++ spec/features/auth/sign_out_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+feature 'User sign out', %q{
+  In order to sign in with different creds
+  As a user
+  I want to be able to sign out
+} do
+
+  scenario 'Authenticated user try to sign out' do
+    @user = create(:user)
+    sign_in(@user)
+
+    visit root_path
+    click_on('Sign out')
+
+    expect(page).to have_content('Signed out successfully.')
+    expect(current_path).to eq root_path
+  end
+end
